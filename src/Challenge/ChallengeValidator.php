047140792e6f6009d080e8f56db03d053214d956
Michael Dance <michael.dance@gmail.com>
2016-01-31 01:13:04 +0700

    Added version 2 signature validation support

--- src/Challenge/ChallengeValidator.php
+++ src/Challenge/ChallengeValidator.php
@@ -9,6 +9,7 @@
 namespace Drupal\trezor_connect\Challenge;
 
 use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+use Symfony\Component\Process\Exception\LogicException;
 
 class ChallengeValidator implements ChallengeValidatorInterface {
 
@@ -46,22 +47,37 @@ class ChallengeValidator implements ChallengeValidatorInterface {
 
   /**
    * @inheritDoc
+   *
+   * Adapted from:
+   * https://github.com/trezor/connect/blob/gh-pages/examples/server.php
    */
   public function validate() {
     $output = FALSE;
 
-    // Check challenge response matches the challenge
-    // Check challenge response
-
     $challenge = $this->getChallenge();
+
     $response = $this->getChallengeResponse();
 
-    $challenge_hidden = $challenge->getChallengeHidden();
-    $challenge_hidden = hex2bin($challenge_hidden);
+    $version = $response->getVersion();
 
+    $challenge_hidden = $challenge->getChallengeHidden();
     $challenge_visual = $challenge->getChallengeVisual();
 
     $signature = $response->getSignature();
+
+    if ($version == 1) {
+      $challenge_hidden = hex2bin($challenge_hidden);
+    }
+    else if ($version == 2) {
+      $challenge_hidden = hex2bin($challenge_hidden);
+      $challenge_hidden = hash('sha256', $challenge_hidden, TRUE);
+
+      $challenge_visual = hash('sha256', $challenge_visual, TRUE);
+    }
+    else {
+      // TODO: Document thrown exception in docblock
+      throw new LogicException('The challenge response version is unknown.');
+    }
 
     $message = $challenge_hidden . $challenge_visual;
 
@@ -79,15 +95,9 @@ class ChallengeValidator implements ChallengeValidatorInterface {
 
     $hash = $ecdsa->hash256($data);
 
-    $result = $ecdsa->checkSignaturePoints($this->public_key, $R, $S, $hash);
+    $public_key = $response->getPublicKey();
 
-    if ($result) {
-      $result = $ecdsa->getAddress($this->public_key);
-
-      if ($result == $this->address) {
-        $output = TRUE;
-      }
-    }
+    $output = $ecdsa->checkSignaturePoints($public_key, $R, $S, $hash);
 
     return $output;
   }
