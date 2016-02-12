e998aa22ce2ac24beeb84bfef4b8dfae50b2a5d3
Michael Dance <michael.dance@gmail.com>
2016-02-12 11:00:19 +0700

    Added challenge_response_backend settings

---
+++ src/Compiler/ChallengeBackendsPass.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\trezor_connect\Compiler\ChallengeBackendsPass.
+ */
+
+namespace Drupal\trezor_connect\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Adds trezor_connect_challenge_backends parameter to the container.
+ */
+class ChallengeBackendsPass implements CompilerPassInterface {
+
+  /**
+   * Implements CompilerPassInterface::process().
+   *
+   * Collects the tagged services and stores them into the appropriate parameter.
+   */
+  public function process(ContainerBuilder $container) {
+    $services = array();
+    $default = NULL;
+
+    $definitions = $container->findTaggedServiceIds('trezor_connect_challenge_backend');
+
+    foreach ($definitions as $key => $value) {
+      if (is_null($default)) {
+        $default = $key;
+      }
+
+      if (isset($value[0]['default'])) {
+        $default = $key;
+      }
+
+      $definition = $container->getDefinition($key);
+
+      $tag = $definition->getTag('trezor_connect_challenge_backend');
+
+      $services[$key] = array(
+        'class' => $definition->getClass(),
+        'title' => $tag['0']['title'],
+      );
+    }
+
+    $container->setParameter('trezor_connect_challenge_backends', $services);
+    $container->setParameter('trezor_connect_challenge_backend', $default);
+  }
+}
--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -35,10 +35,16 @@ class SettingsForm extends ConfigFormBase {
    * @param \Drupal\Core\State\StateInterface $state
    *   The state keyvalue collection to use.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, StateInterface $state, $mapping_backends, $mapping_backend, TrezorConnectInterface $trezor_connect) {
+  public function __construct(ConfigFactoryInterface $config_factory, StateInterface $state, $challenge_backends, $challenge_backend, $challenge_response_backends, $challenge_response_backend, $mapping_backends, $mapping_backend, TrezorConnectInterface $trezor_connect) {
     parent::__construct($config_factory);
 
     $this->state = $state;
+
+    $this->challenge_backends = $challenge_backends;
+    $this->challenge_backend = $challenge_backend;
+
+    $this->challenge_response_backends = $challenge_response_backends;
+    $this->challenge_response_backend = $challenge_response_backend;
 
     $this->mapping_backends = $mapping_backends;
     $this->mapping_backend = $mapping_backend;
@@ -53,6 +59,10 @@ class SettingsForm extends ConfigFormBase {
     return new static(
       $container->get('config.factory'),
       $container->get('state'),
+      $container->getParameter('trezor_connect_challenge_backends'),
+      $container->getParameter('trezor_connect_challenge_backend'),
+      $container->getParameter('trezor_connect_challenge_response_backends'),
+      $container->getParameter('trezor_connect_challenge_response_backend'),
       $container->getParameter('trezor_connect_mapping_backends'),
       $container->getParameter('trezor_connect_mapping_backend'),
       $container->get('trezor_connect')
@@ -134,6 +144,56 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $default_value,
     );
 
+    $key = 'challenge_backend';
+
+    $description = t('Please specify the challenge backend to use.');
+
+    $options = $this->trezor_connect->challengeBackendOptions();
+
+    $default_value = $this->challenge_backend;
+
+    $result = $config->get($key);
+
+    if ($result) {
+      $default_value = $result;
+    }
+
+    $form[$key] = array(
+      '#type' => 'radios',
+      '#required' => TRUE,
+      '#title' => t('Challenge Backend'),
+      '#options' => $options,
+      '#empty_value' => '',
+      '#empty_option' => t('Select an option'),
+      '#description' => $description,
+      '#default_value' => $default_value,
+    );
+
+    $key = 'challenge_response_backend';
+
+    $description = t('Please specify the challenge response backend to use.');
+
+    $options = $this->trezor_connect->challengeResponseBackendOptions();
+
+    $default_value = $this->challenge_response_backend;
+
+    $result = $config->get($key);
+
+    if ($result) {
+      $default_value = $result;
+    }
+
+    $form[$key] = array(
+      '#type' => 'radios',
+      '#required' => TRUE,
+      '#title' => t('Challenge Response Backend'),
+      '#options' => $options,
+      '#empty_value' => '',
+      '#empty_option' => t('Select an option'),
+      '#description' => $description,
+      '#default_value' => $default_value,
+    );
+
     $key = 'mapping_backend';
 
     $description = t('Please specify the mapping backend to use.');
@@ -173,6 +233,8 @@ class SettingsForm extends ConfigFormBase {
       'external',
       'url',
       'callback',
+      'challenge_backend',
+      'challenge_response_backend',
       'mapping_backend',
     );
 
