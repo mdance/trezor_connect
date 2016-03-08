a4f03df3e166c07452b94654cc4520d711e3c9c1
Michael Dance <michael.dance@gmail.com>
2016-03-08 07:59:00 +0700

    Added Messages constants

---
+++ src/Enum/Messages.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Contains Drupal\trezor_connect\Enum\Messages.
+ */
+
+namespace Drupal\trezor_connect\Enum;
+
+class Messages {
+  // TODO: Map messages to constants
+
+  /**
+   * Provides the password empty message.
+   */
+  const PASSWORD_EMPTY = 'Please specify the current password for the account in order to make changes to the authentication device';
+
+  /**
+   * Provides the invalid password message.
+   */
+  const PASSWORD_INVALID = 'The password you have entered is invalid.';
+
+  /**
+   * Provides the max password attempts message.
+   */
+  const PASSWORD_MAX_ATTEMPTS = 'You are not allowed anymore password guesses.';
+
+  /**
+   * Provides the invalid challenge response message.
+   */
+  const CHALLENGE_RESPONSE_INVALID = 'An error has occurred validating your TREZOR credentials.';
+
+  /**
+   * Provides the max challenge response attempts message.
+   */
+  const CHALLENGE_RESPONSE_MAX_ATTEMPTS = 'You are not allowed anymore authentication device attempts.';
+
+}
